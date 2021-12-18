76ac6ebeff42786d1a854103d9bfc9708cfeca99
shahrameshv <shahrameshv@gmail.com>
2021-12-18 18:23:29 +0530

    added new 3 ml model to code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("This is model branch file")
+
+
+def model1();
+    print("adding code to model 1")
+
+def model2();
+    print("adding code to model 2")
+
+def model3();
+    print("adding code to model 3")
